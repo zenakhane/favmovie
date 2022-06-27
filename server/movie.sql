db0e503401db85e82d07db19483a7fc65d6f475f
zenakhane <lilliantyiso@gmail.com>
2022-06-27 15:38:52 +0200

    dabase tables done

--- server/movie.sql
+++ server/movie.sql
@@ -1,8 +1,7 @@
 create table movies(
   id serial not null primary key, 
-  user_id text, 
-  movie_id varchar(), 
-  PRIMARY KEY(user_id), 
-  CONSTRAINT username FOREIGN KEY(user_id) REFERENCES users(username)
+  user_id int, 
+  movie_id INT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
