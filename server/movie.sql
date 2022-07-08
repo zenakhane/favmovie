10e39b69305b30d3a217be42cf18ecf6190e10a6
zenakhane <lilliantyiso@gmail.com>
2022-07-08 10:46:57 +0200

    removed all silly code

--- server/movie.sql
+++
@@ -1,7 +0,0 @@
-create table movies(
-  id serial not null primary key, 
-  user_id int, 
-  movie_id INT NOT NULL,
-  FOREIGN KEY(user_id) REFERENCES users(id)
-);
-
--- server/users.sql
+++
@@ -1,5 +0,0 @@
-create table users(
-    id serial not null primary key,
-	username text,
-    password text,
-);
